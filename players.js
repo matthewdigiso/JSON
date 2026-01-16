31a573755f65f0257b8d8ee3cee4c8a1420c7f8f
Matthew DiGiso <126mdigiso@frhsd.com>
2026-01-16 08:38:01 -0500

    commit and push

--- players.js
+++ players.js
@@ -3,9 +3,26 @@ const xhr = new XMLHttpRequest();
 
 // onload is an Event... like a click event :)
 xhr.onload = function() {
-    if(xhr.status === 200) {
-        const date = JSON.parse(xhr.responseText);
+    if(xhr.status === 200) {        const data = JSON.parse(xhr.responseText);
+        const data = JSON.parse(xhr.responseText);
+        const players;
+        const container;
+
+players.forEach(player => {
+    const card;
+})
+
+        
+
+        // let const statsHTML = "";
+        // let const teamsHTML = "";
+        // let const awardHTML = "";
+
+        // for(let team=0;team<players.teams.length;team++) 
+    }
 };
 
 xhr.open("GET", "players.json");
 xhr.send();
+
+data.players;
